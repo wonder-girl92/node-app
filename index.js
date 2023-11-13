fd6e3a8a950e6f6b83547d2d06215368caad6f2d
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-11-13 23:26:37 +0300

    writeFile

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const yargs = require('yargs')
+const pkg = require('./package.json')
+const {addNote, getNotes} = require('./notes.controller')
+
+yargs.version(pkg.version)
 
 yargs.command({
   command: 'add',
@@ -11,7 +15,7 @@ yargs.command({
     }
   },
   handler({ title }) {
-    console.log('Add command:', title)
+   addNote(title)
   }
 })
 
@@ -19,7 +23,8 @@ yargs.command({
   command: 'list',
   describe: 'Print all notes',
   handler() {
-    console.log('List command')
+    const notes = getNotes()
+    console.log(notes)
   }
 })
 
