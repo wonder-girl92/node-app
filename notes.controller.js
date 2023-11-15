3124fcb03dbef95a031db7db4971bf98b322870c
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-11-15 21:47:43 +0300

    fs, path

--- notes.controller.js
+++ notes.controller.js
@@ -1,19 +1,27 @@
 const fs = require('fs/promises')
+const path = require('path')
+
+const notesPath = path.join(__dirname, 'db.json')
+console.log(notesPath)
 
 async function addNote(title){
-
-  const notes = require('./db.json')
-
-const note =  {
+  const notes = await getNotes()
+  const note = {
   title,
   id: Date.now().toString()
 }
 notes.push(note)
-  await fs.writeFile('./db.json', JSON.stringify(notes))
+
+  await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
-function getNotes(){
- return require('./db.json')
+addNote()
+
+
+async function getNotes(){
+  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+
 }
 
 module.exports = {
