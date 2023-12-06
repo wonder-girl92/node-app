3f81c4230f1cbb0d5d1a80e9ba039479e1e0f901
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-12-06 20:53:02 +0300

    http.createserver gets html by fs.readfile

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const http = require('http')
 const chalk = require('chalk')
+const fs = require('fs/promises')
+const path = require('path')
 
 const port = 3000
 
-const server = http.createServer((req, res) => {
-  console.log('Request method:', req.method)
-  console.log('Request url:', req.url)
+const basePath = path.join(__dirname, 'pages')
 
-  res.end('Hello from server!!!')
+const server = http.createServer(async (req, res) => {
+ if (req.method === 'GET'){
+  const content = await fs.readFile(path.join(basePath, 'index.html'))
+   // res.setHeader('Content-Type', 'text/html')
+   res.writeHead(200, {
+     'Content-Type': 'text/html'
+   })
+   res.end(content)
+ }
 })
 
 server.listen(port, () => {
