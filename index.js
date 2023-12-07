42ef9cec8c1a502a52f17fe4b8c9edcbd3b76ef5
wonder-girl92 <81470730+wonder-girl92@users.noreply.github.com>
2023-12-07 23:58:21 +0300

    remove data at client and server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const chalk = require('chalk')
-const {addNote, getNotes} = require('./notes.controller')
+const path = require('path')
+const {addNote, getNotes, removeNote} = require('./notes.controller')
 
 const port = 3000
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'pages')
 
+app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(express.urlencoded({
   extended: true
 }))
@@ -15,7 +17,8 @@ app.use(express.urlencoded({
 app.get('/', async (req, res) => {
   res.render('index', {
     title: 'Express App',
-    notes: await getNotes()
+    notes: await getNotes(),
+    created: false
   })
 })
 
@@ -23,7 +26,17 @@ app.post('/', async (req, res) => {
  await addNote(req.body.title)
   res.render('index', {
     title: 'Express App',
-    notes: await getNotes()
+    notes: await getNotes(),
+    created: true
+  })
+})
+
+app.delete('/:id', async(req, res) => {
+ await removeNote(req.params.id)
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
   })
 })
 
